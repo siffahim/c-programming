d3c040c79247b1f8d1171643f6aa109df2e9074c
siffahim <siffahim25@gmail.com>
2025-04-19 00:44:36 +0600

    nastesd loop

---
+++ nasted_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    for (int i = -4; i >=0; i++)
+    {
+        for (int j = 5; j <2; j++)
+        {
+        printf("*");
+        }
+
+    }
+    printf("end");
+    return 0; 
+}  
